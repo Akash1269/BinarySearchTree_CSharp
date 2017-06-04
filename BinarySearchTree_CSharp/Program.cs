60796bc0fbc9774386489a82aca4456e6d4abf8c
Akash Bhayekar <akashbhayekar@hotmail.com>
2017-06-04 11:14:21 +0530

    Initial Commit with all the operations on BST

--- BinarySearchTree_CSharp/Program.cs
+++ BinarySearchTree_CSharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BinarySearchTree_CSharp
 {
@@ -10,6 +6,86 @@ namespace BinarySearchTree_CSharp
     {
         static void Main(string[] args)
         {
+            Tree tree = new Tree();
+            Console.WriteLine("\nWelcome to BST Data strucutre\n");
+            Console.WriteLine("\nPlease enter the elements in the space seperated format\n");
+            int[] dataArr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            foreach (int item in dataArr)
+            {
+                tree.Insert(item);
+            }
+
+            int choice = -1;
+            int data = 0;
+            while(choice != 0)
+            {
+                Console.WriteLine("\n\n1.Insert\n2.Pre Order\n3.In Order\n4.Post Order\n5.Number of Nodes\n" +
+                    "6.Height of the tree\n7.No of leaves\n8.Mirror image tree\n9.Copy Tree\n10.Find Node\n11.BFS" + 
+                    "\n12.Bredth Of the tree\n13.Delete\n");
+                Console.WriteLine("\nPlease enter the choice\n");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\nPlease enter the element to be inserted in the tree\n");
+                        data = int.Parse(Console.ReadLine());
+                        tree.Insert(data);
+                        break;
+                    case 2:
+                        Console.WriteLine("\nPre-order of the tree\n");
+                        tree.PreOrder();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nIn-order of the tree\n");
+                        tree.InOrder();
+                        break;
+                    case 4:
+                        Console.WriteLine("\nPost-order of the tree\n");
+                        tree.PostOrder();
+                        break;
+                    case 5:
+                        Console.WriteLine(tree.NumOfNodes());
+                        break;
+                    case 6:
+                        Console.WriteLine(tree.Height());
+                        break;
+                    case 7:
+                        Console.WriteLine(tree.NumLeaves());
+                        break;
+                    case 8:
+                        tree.MirrorImage();
+                        break;
+                    case 9:
+                        Console.WriteLine("\nCopy of the tree\n");
+                        tree.TreeCopy();
+                        break;
+                    case 10:
+                        Console.WriteLine("\nPlease enter the element to be searched in the tree\n");
+                        data = int.Parse(Console.ReadLine());
+                        if (tree.FindNode(data)) { Console.WriteLine("Found"); } else { Console.WriteLine("Not Found"); }
+                        break;
+                    case 11:
+                        Console.WriteLine("\nBFS of the tree\n");
+                        tree.BFS();
+                        break;
+                    case 12:
+                        Console.WriteLine("\nBreadth of the tree\n");
+                        Console.WriteLine(tree.Breadth());
+                        break;
+                    case 13:
+                        Console.WriteLine("\nPlease enter the element to be deleted in the tree\n");
+                        data = int.Parse(Console.ReadLine());
+                        if (tree.Delete(data)) { Console.WriteLine("Deleted"); } else { Console.WriteLine("Not Found To be Deleted"); }
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("\nIn-order of the tree\n");
+                tree.InOrder();
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ BinarySearchTree_CSharp/Tree.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_CSharp
+{
+    class Tree
+    {
+        public Node Root { get; set; }
+
+        public bool Insert(int data)
+        {
+            Node node = new Node() { Data = data };
+            if (Root == null)
+                Root = node;
+            bool inserted = false;
+            Node temp = Root;
+            while (!inserted)
+            {
+                if(data < temp.Data)
+                {
+                    if(temp.Left == null)
+                    {
+                        temp.Left = node;
+                        inserted = true;
+                    }
+                    else
+                    {
+                        temp = temp.Left;
+                    }
+                }
+                else if (data > temp.Data)
+                {
+                    if (temp.Right == null)
+                    {
+                        temp.Right = node;
+                        inserted = true;
+                    }
+                    else
+                    {
+                        temp = temp.Right;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void Visit(Node node)
+        {
+            Console.Write(node.Data + " ");
+        }
+
+        public void PreOrder()
+        {
+            _PreOrder(Root);
+        }
+
+        void _PreOrder(Node node)
+        {
+            if (node != null)
+            {
+                Visit(node);
+                _PreOrder(node.Left);
+                _PreOrder(node.Right);
+            }
+        }
+
+        public void InOrder()
+        {
+            _InOrder(Root);
+        }
+
+
+        void _InOrder(Node node)
+        {
+            if (node != null)
+            {
+                _InOrder(node.Left);
+                Visit(node);
+                _InOrder(node.Right);
+            }
+        }
+
+        public void PostOrder()
+        {
+            _PostOrder(Root);
+        }
+
+        void _PostOrder(Node node)
+        {
+            if (node != null)
+            {
+                _PostOrder(node.Left);
+                _PostOrder(node.Right);
+                Visit(node);
+            }
+        }
+
+        public int NumOfNodes()
+        {
+            return _NumOfNodes(Root);
+        }
+
+        int _NumOfNodes(Node node)
+        {
+            if (node == null)
+                return 0;
+            return _NumOfNodes(node.Left) + _NumOfNodes(node.Right) + 1;
+        }
+
+        public int Height()
+        {
+            return _Height(Root);
+        }
+
+        int _Height(Node node)
+        {
+            if (node == null)
+                return -1;
+            int leftHeight = _Height(node.Left);
+            int RightHeight = _Height(node.Right);
+            return (leftHeight > RightHeight ? leftHeight : RightHeight) + 1;
+        }
+        public int NumLeaves()
+        {
+            return _NumLeaves(Root);
+        }
+
+        int _NumLeaves(Node node)
+        {
+            if (node == null)
+                return 0;
+            if (node.Left == null && node.Right == null)
+                return 1;
+            return _NumLeaves(node.Left) + _NumLeaves(node.Right);
+        }
+
+        public void MirrorImage()
+        {
+            _MirrorImage(Root);
+        }
+        void _MirrorImage(Node node)
+        {
+            if (node != null)
+            {
+                Node temp = node.Left;
+                node.Left = node.Right;
+                node.Right = temp;
+                _MirrorImage(node.Left);
+                _MirrorImage(node.Right);
+            }
+        }
+
+        public void TreeCopy()
+        {
+            Node newTree = _TreeCopy(Root);
+            _InOrder(newTree);
+        }
+        Node _TreeCopy(Node node)
+        {
+            if (node != null)
+                return new Node() { Data = node.Data, Left = _TreeCopy(node.Left), Right = _TreeCopy(node.Right) };
+            else
+                return null;
+        }
+
+        public bool FindNode(int data)
+        {
+            bool found = false;
+            Node node = Root;
+            while(node != null && !found)
+            {
+                if(data > node.Data)
+                {
+                    node = node.Right;
+                }
+                else if (data < node.Data)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    found = true;
+                }
+            }
+            return found;
+        }
+
+        public void BFS()
+        {
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(Root);
+
+            while(q.Count > 0)
+            {
+                Node node = q.Dequeue();
+                Visit(node);
+                if (node.Left != null)
+                {
+                    q.Enqueue(node.Left);
+                }
+                if (node.Right != null)
+                {
+                    q.Enqueue(node.Right);
+                }
+            }
+        }
+
+        public int Breadth()
+        {
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(Root);
+            int max = 0;
+
+            while (q.Count > 0)
+            {
+                if (max < q.Count)
+                    max = q.Count;
+                Node node = q.Dequeue();
+                if (node.Left != null)
+                {
+                    q.Enqueue(node.Left);
+                }
+                if (node.Right != null)
+                {
+                    q.Enqueue(node.Right);
+                }
+            }
+            return max;
+        }
+
+        public bool Delete(int data)
+        {
+            Node node = Root;
+            Node prev = Root;
+            bool deleted = false;
+            Node newNode = null;
+
+            while(node != null && !deleted)
+            {
+                if(data > node.Data)
+                {
+                    prev = node;
+                    node = node.Right;                    
+                }
+                else if(data < node.Data)
+                {
+                    prev = node;
+                    node = node.Left;
+                }
+                else
+                {
+                    DeleteNode(ref newNode, node);
+                    deleted = true;
+                    if (prev.Right != null && prev.Right.Data == node.Data)
+                    {
+                        prev.Right = newNode;
+                    }
+                    else
+                    {
+                        prev.Left = newNode;
+                    }
+                }
+            }
+
+            return deleted;
+        }
+
+        void DeleteNode(ref Node newNode, Node node)
+        {            
+            if(node.Left == null)
+            {
+                newNode = node.Right;                
+            }
+            else if (node.Right == null)
+            {
+                newNode = node.Left;
+            }
+
+            Case A: Where we pull out the right most node of left subtree of the node to be deleted as
+             its the highest node just before current ndoe then point right tree to the right of that node
+            else
+            {
+                Node temp = node.Left;
+                while (temp.Right != null)
+                {
+                    temp = temp.Right;
+                }
+                temp.Right = node.Right;
+                newNode = node.Left;
+            }
+            if (Root.Data == node.Data)
+            {
+                Root = node.Left;
+            }
+
+            //Case B: pull out the left most node of the right subtree of node to be deleted and point left subtree to the left
+            // of that node, this element just next to current element
+            //else
+            //{
+            //    Node temp = node.Right;
+            //    while (temp.Left != null)
+            //    {
+            //        temp = temp.Left;
+            //    }
+            //    temp.Left = node.Left;
+            //    newNode = node.Right;
+            //}
+            //if (Root.Data == node.Data)
+            //{
+            //    Root = node.Right;
+            //}
+
+            //Case C: find the right most element of left subtree of node to be deleted and then swap the values of
+            // right most ndoe and node to be deleted, as we have found the replacement and then join left of that right most node
+            // to right of its parent
+        }
+    }
+}
